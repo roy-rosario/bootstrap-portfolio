dd691464243e279e5004ec8e6bae058fb98b970f
Roy <roy.rosario88@gmail.com>
2022-03-30 13:04:04 -0400

    start of header dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar'
+import Header from './components/Header'
 
 function App() {
   return (
     <div >
       <NavBar/>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Header = () =>{
+    return(
+        <div className="header-wrapper">
+            <div className='main-info'>
+                <h1>Web development and website promotions</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
