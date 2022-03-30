cbd0c8b87dcc632c70248b238a5baa899a84b0ff
Roy <roy.rosario88@gmail.com>
2022-03-30 14:08:27 -0400

    adding particle effects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../App.css'
+import Typed from 'react-typed'
 
 const Header = () =>{
     return(
         <div className="header-wrapper">
             <div className='main-info'>
                 <h1>Web development and website promotions</h1>
+                <Typed
+                    className='typed-text'
+                    strings={['Front-End Design', 'Web Development', 'Facebook Ads SMM', 'Google Ads']} 
+                    typeSpeed={50}
+                    backSpeed={30}
+                    loop
+                />
+                <a href="#" className='btn-main-offer'>contact us</a>
             </div>
         </div>
     )
